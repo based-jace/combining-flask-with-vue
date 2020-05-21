90cae3b79c5b9076975f0c5165e0710d4d4d1a59
based-jace <jacemedlin@gmail.com>
2020-05-21 08:52:30 -0500

    First draft complete

---
+++ 2. Separate Ends/webapp/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="app">
+    <p>{{ greeting }}</p>
+    <p>{{ flaskGreeting }}</p>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    data: function(){
+        return {
+            greeting: 'Hello, Vue!',
+            flaskGreeting: ""
+        }
+    },
+    created: async function(){
+        const gResponse = await fetch("http://localhost:5000/greeting");
+        const gObject = await gResponse.json();
+        this.setFlaskGreeting = gObject.greeting;
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ 2.5. Separate with SSR/webapp/pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <p>{{ greeting }}</p>
+	<p>{{ flaskGreeting }}</p>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+	components: {
+		Logo
+	},
+	data: function(){
+		return {
+			greeting: 'Hello, Vue!',
+			flaskGreeting: ''
+		}
+	},
+	created: async function(){
+		const gResponse = await fetch("http://localhost:5000/greeting");
+		const gObject = await gResponse.json();
+		this.flaskGreeting = gObject.greeting;
+	}
+}
+</script>
+
+<style>
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
